db8a4c63ecdeba5e7b1adeea882748f8d2cbecde
Roy Varghese Mathew <vargs77@cs.washington.edu>
2018-04-27 12:51:21 -0700

    modularizes socket.io and adds comments

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Dependencies
 console.log(": Importing Modules");
-var express = require('express');
-var request = require("request");
-var http = require("http");
-var https = require('https');
-var fs = require("fs");
-var mailHelper = require('./lib/mail_helper');
+const express = require('express');
+const request = require("request");
+const http = require("http");
+const https = require('https');
+const fs = require("fs");
+const mailHelper = require('./lib/mail_helper');
+const socketWrapper = require('./lib/socket_wrapper');
 
 console.log(": Before running this file, make sure the values in settings.json are correct.");
 
@@ -22,31 +23,13 @@ var emailInfo = settings.email;
 emailInfo.from = '"Wick-It Grip" <wickitgrip@gmail.com>';
 emailInfo.attachments = null;
 
-var mailHashSet = {
-  addEmail: function(email) {
-    this[email.toLowerCase()] = true;
-  },
-
-  checkEmail: function(email) {
-    return this[email.toLowerCase()] == true
-  }
-}
-
-console.log(": importing subscribers")
-fs.readFileSync("./data/subscribers.txt").toString().split('/n').forEach(
-  (line) => {
-    console.log(":    importing subscriber " + line);
-    mailHashSet.addEmail(line.trim());
-  }
-)
-
 console.log(": Setting Up Server!")
 var server;
 
 // create express app
 var app = express();
 
-if (settings.live) {
+if (settings.live) {  // On actual server
   // SSL Info Setup
   console.log(': Reading SSL Setup PEMs')
   var privateKey  = fs.readFileSync('/etc/letsencrypt/live/wickitgrip.com/privkey.pem');
@@ -67,7 +50,7 @@ if (settings.live) {
   server = https.createServer(credentials, app).listen(port, function() {
     console.log(': Server is listening on port ' + port);
   });
-} else {
+} else {  // for local development
   // Setup Express
   console.log(": Creating express server");
   app.use(express.static('public'));
@@ -77,86 +60,13 @@ if (settings.live) {
   });
 }
 
-// socket.io Code!!
-const io = require('socket.io')(server);
-function onConnection(socket){
-  socket.on('subscription', (data) => {
-    console.log(": received subscription with data: " + JSON.stringify(data))
-
-    if (data.message == undefined) {
-      console.log(": No input email detected!");
-      return;
-    }
-    if (!mailHelper.validateEmail(data.message)) {
-      console.log(": invalid email detected!");
-      return;
-    }
-    if (mailHashSet.checkEmail(data.message)) {
-      console.log(": This email has already been subscribed!");
-      return;
-    }
-
-    fs.appendFile("data/subscribers.txt", '\n' + data.message.trim(), (err) => {
-      if (err) {
-        console.log(": subscr error!");
-      }
-
-      console.log(": Sending confirmation email to self!")
-      emailInfo.to = 'wickitgrip@gmail.com';
-      emailInfo.subject =  'Subscription Update';
-      emailInfo.text = data.message.trim() + ' has subscribed!';
-      emailInfo.attachments = [{
-        filename: 'subscribers.txt',
-        path: './data/subscribers.txt'
-      }]
-      mailHelper.send(emailInfo);
-      
-      console.log(": Sending confirmation email to user!")
-      emailInfo.to = data.message.trim();
-      emailInfo.subject =  'Wick-It Grip Subscription Confirmation';
-      emailInfo.text = "Hello,\n\nThis email has been subscribed to Wick-It Grip's " +
-        "mailing list. For more info go to wickitgrip.com.\n" + 
-        "If this was a mistake, please email us back to unsubscribe.\n\n" + 
-        "From,\nThe Wick-It Grip Team";
-      emailInfo.attachments = null;
-      mailHelper.send(emailInfo);
-      
-      mailHashSet.addEmail(data.message)
-      
-    })
-  });
-
-  socket.on('message', (data) => {
-    console.log(": received message with data: " + JSON.stringify(data))
-
-    if (data.email == undefined || data.name == undefined || data.subject == undefined
-      || data.message == undefined) {
-      console.log(": message data incomplete!")
-      return;
-    }
-    if (!mailHelper.validateEmail(data.email)) {
-      console.log(": invalid email detected!");
-      return;
-    }
-
-    fs.appendFile("data/messages.txt", '\n' + JSON.stringify(data), (err) => {
-      if (err) {
-        console.log(": messages.txt opening error:" + err);
-      }
-
-      console.log(": Sending message email to self!")
-      emailInfo.to = 'wickitgrip@gmail.com';
-      emailInfo.subject =  "Website MSG: " + data.subject;
-      emailInfo.text = data.message;
-      emailInfo.attachments = null;
-      mailHelper.send(emailInfo);
-    })
-  });
-}
-io.on('connection', onConnection);
-
+// Default page
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
 
+console.log(": importing subscribers")
+var subscribers = fs.readFileSync("./data/subscribers.txt").toString().split('\n');
+
+const io = socketWrapper(server, subscribers, mailHelper, emailInfo);
 console.log(': index.js end of file reached!!')
--- public/js/message.js
+++ public/js/message.js
@@ -1,41 +1,52 @@
-var socket = io();
+/*
+ * This javascript code represents the client code that will handle sending
+ * email and subscription information to the server.
+ */
 
-var subscribeButton = document.getElementById("ping-button");
-subscribeButton.onclick = function() {  // change event if necessary
-      // build up dictionary to send to server
-      var emailBox = document.getElementById("email-box");
+// socket to host server
+const socket = io();
 
-      dataToSend = {}
-      dataToSend.message = emailBox.value;
+// DOM components that will be used for subscription functionality
+const subscribeButton = document.getElementById("ping-button");
+const emailBox = document.getElementById("email-box");
+const subscribePrompt = document.getElementById("sub-message");
 
-      var resultMessage = "Failed to subscribe"
+// DOM components that will be used for messaging functionality
+const submitButton= document.getElementById("submit-button");
+const submitPrompt= document.getElementById("submit-result");
 
-      if (dataToSend.message != '') {
-          // send data to server
-          socket.emit("subscription", dataToSend)
-          resultMessage = "Sucessfully Subscribed!"
-      }
-      var subMsg = document.getElementById("sub-message");
-      
-      subMsg.innerText = resultMessage;
+// Sends subscription email address to server and updates
+// the subscriber prompt for the user to see the result
+subscribeButton.onclick = function() {
+    let dataToSend = {
+        message: emailBox.value
+    }
+
+    let resultMessage = "Failed to subscribe"
+    if (dataToSend.message != '') {
+        // send data to server
+        socket.emit("subscription", dataToSend)
+        resultMessage = "Successfully Subscribed!"
+    }
+    subscribePrompt.innerText = resultMessage;
 }
 
-var submitButton= document.getElementById("submit-button");
-submitButton.onclick = function() {  // change event if necessary
-    // build up dictionary to send to server
-    var emailBox = document.getElementById("contactEmail");
-    var nameBox = document.getElementById("contactName");
-    var subjectBox = document.getElementById("contactSubject");
-    var messageBox = document.getElementById("contactMessage");
+submitButton.onclick = function() {
+    // Data fields where input is taken from
+    const emailBox = document.getElementById("contactEmail");
+    const nameBox = document.getElementById("contactName");
+    const subjectBox = document.getElementById("contactSubject");
+    const messageBox = document.getElementById("contactMessage");
 
+    // Build Data to send to the server
     dataToSend = {}
     dataToSend.email = emailBox.value;
     dataToSend.name = nameBox.value;
     dataToSend.subject = subjectBox.value;
     dataToSend.message = messageBox.value;
 
-    var resultMessage = ""
-
+    // Parse though different error possibilities
+    let resultMessage = ""
     if (dataToSend.email == '') {
         resultMessage = "Please enter an email.";
     } else if (dataToSend.name == '') {
@@ -46,12 +57,9 @@ submitButton.onclick = function() {  // change event if necessary
         resultMessage = "Please enter an message.";
     } else if (dataToSend.email.indexOf('@') == -1) {
         resultMessage = "Please enter a valid email address."
-    } else {
-        // send data to server
+    } else {  // No error, will send data to server
         socket.emit("message", dataToSend);
-        resultMessage = "Sucessfully Sent Message!";
+        resultMessage = "Successfully Sent Message!";
     }
-
-    var subRes= document.getElementById("submit-result");
-    subRes.innerText = resultMessage;
+    submitPrompt.innerText = resultMessage;
 }    
